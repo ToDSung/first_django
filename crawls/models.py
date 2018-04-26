a4751df1328e0124813b4469460558440e8f04d9
mingchien\ci <wlunareve@gmail.com>
2018-04-26 17:46:05 +0800

    build django model for save facebook crawl data

--- crawls/models.py
+++ crawls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class FanPage(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    time = models.TimeField('time crawled', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class article(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    fanpage = models.ForeignKey(FanPage, on_delete=models.CASCADE)
+    text = models.TextField(blank=True)
+    time = models.DateField('time published')
+
+    def __str__(self):
+        return self.text
