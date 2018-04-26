efbe1566fe992e9bbf94d95e317aec890223f4e2
mingchien\ci <wlunareve@gmail.com>
2018-04-26 11:25:22 +0800

    use django.views

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse,HttpResponseRedirect, Http404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # Under this line we use to change the object name corresponding temlate html
+    #context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #output = ', '.join([q.question_text for q in latest_question_list])
@@ -28,7 +46,7 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
+'''
 def vote (request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
