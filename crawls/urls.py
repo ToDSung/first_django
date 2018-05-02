56069242ddebd3db87ae4ecb1ac9c6719630acec
mingchien\ci <wlunareve@gmail.com>
2018-05-02 16:57:47 +0800

    add detail page to see all text in the appointed fanpage

--- crawls/urls.py
+++ crawls/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.urls import path
 
 from . import views
@@ -5,4 +6,6 @@ from . import views
 app_name = 'crawls'
 urlpatterns = [
     path('', views.FanPageView.as_view(), name='index'),
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('fbcrawl', views.fbcrawl, name='fbcrawl'),
     ]
--- crawls/views.py
+++ crawls/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
-from .models import FanPage
+from .models import FanPage, Article
+from .fb_crawler import fb_crawler
 # Create your views here.
 
 class FanPageView(generic.ListView):
@@ -11,3 +13,16 @@ class FanPageView(generic.ListView):
     def get_queryset(self):
         fanpage_list = FanPage.objects.all()
         return fanpage_list
+
+class DetailView(generic.DetailView):
+    model = FanPage
+    template_name = 'crawls/detail.html'
+
+def fbcrawl(request):
+    imformation_list = fb_crawler()
+    
+    for row in imformation_list:
+        Article.objects.create(fanpage_id=1,text=row[0], time=row[1])
+    Article.save
+    
+    return HttpResponse ("The fb crawler are already finished!")
