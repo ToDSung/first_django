aa7d23af81c489101dbfb38524604bf99a22eca7
mingchien\ci <wlunareve@gmail.com>
2018-05-10 15:22:44 +0800

    Separate ptt and facebook url link

--- crawls/urls.py
+++ crawls/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -20,23 +20,31 @@ urlpatterns = [
 '''
 
 app_name = 'crawls'
-urlpatterns = [
-    path('facebook', views.FanPageView.as_view(), name='facebook_index'),
-    path('facebook/add', views.add, name='facebook_add'),
-    path('facebook/<int:pk>', views.DetailView.as_view(), name='facebook_detail'),
-    path('facebook/<int:fanpage_id>/crawl',
+
+facebook_patterns = [
+    path('', views.FanPageView.as_view(), name='facebook_index'),
+    path('add', views.add, name='facebook_add'),
+    path('<int:pk>', views.DetailView.as_view(), name='facebook_detail'),
+    path('<int:fanpage_id>/crawl',
          views.crawl, name='facebook_crawl'),
-    path('facebook/<int:fanpage_id>/delete', views.delete_crawled_data,
+    path('<int:fanpage_id>/delete', views.delete_crawled_data,
          name='facebook_delete_crawled_data'),
-    path('facebook/<int:fanpage_id>/delete2',
+    path('<int:fanpage_id>/delete2',
          views.delete_fanpage, name='facebook_delete_fanpage'),
-    path('ptt', views.BoardView.as_view(), name='ptt_index'),
+]
+ptt_patterns = [
+    path('', views.BoardView.as_view(), name='ptt_index'),
     #path('ptt/<int:pk>', views.PTTDetailView.as_view(), name='ptt_detail'),
-    path('ptt/<int:board_id>', views.show_article, name='ptt_detail'),
-    path('ptt/<int:board_id>/crawl', views.crawl_ptt_data, name='ptt_crawl'),
-    path('ptt/<int:board_id>/delete', views.delete_ptt_data,
+    path('<int:board_id>', views.show_article, name='ptt_detail'),
+    path('<int:board_id>/crawl', views.crawl_ptt_data, name='ptt_crawl'),
+    path('<int:board_id>/delete', views.delete_ptt_data,
          name='ptt_delete_crawled_data'),
-    path('ptt/<int:board_id>/delete2',
+    path('<int:board_id>/delete2',
          views.delete_board, name='ptt_delete_board'),
-    path('ptt/add', views.add_ptt_board, name='ptt_add'),
+    path('add', views.add_ptt_board, name='ptt_add'),
+]
+
+urlpatterns = [
+    path('facebook/', include(facebook_patterns)),
+    path('ptt/', include(ptt_patterns)),
 ]
