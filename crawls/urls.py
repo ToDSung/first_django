a745d480638ebc94e66e0559f4278532d28c73c5
mingchien\ci <wlunareve@gmail.com>
2018-05-10 14:46:28 +0800

    Add the example of url customize method from official doc

--- crawls/urls.py
+++ crawls/urls.py
@@ -3,6 +3,22 @@ from django.urls import path
 
 from . import views
 
+'''
+用這個方法嘗試做一個自己的網址過濾器
+class FourDigitYearConverter:
+    regex = '[0-9]{4}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return '%04d' % value
+register_converter(converters.FourDigitYearConverter, 'yyyy')
+urlpatterns = [
+    path('articles/<yyyy:year>/', views.year_archive),
+]
+'''
+
 app_name = 'crawls'
 urlpatterns = [
     path('facebook', views.FanPageView.as_view(), name='facebook_index'),
@@ -15,11 +31,12 @@ urlpatterns = [
     path('facebook/<int:fanpage_id>/delete2',
          views.delete_fanpage, name='facebook_delete_fanpage'),
     path('ptt', views.BoardView.as_view(), name='ptt_index'),
-    path('ptt/<int:pk>', views.PTTDetailView.as_view(), name='ptt_detail'),
+    #path('ptt/<int:pk>', views.PTTDetailView.as_view(), name='ptt_detail'),
+    path('ptt/<int:board_id>', views.show_article, name='ptt_detail'),
     path('ptt/<int:board_id>/crawl', views.crawl_ptt_data, name='ptt_crawl'),
     path('ptt/<int:board_id>/delete', views.delete_ptt_data,
          name='ptt_delete_crawled_data'),
     path('ptt/<int:board_id>/delete2',
          views.delete_board, name='ptt_delete_board'),
-    path('ptt/add', views.add_ptt_board, name='ptt_add'),     
+    path('ptt/add', views.add_ptt_board, name='ptt_add'),
 ]
