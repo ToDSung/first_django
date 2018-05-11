24aa86dc3bd3e9656f96f7e15b5b5d17e65f0338
mingchien\ci <wlunareve@gmail.com>
2018-05-11 12:25:27 +0800

    Add django restful api to setting

--- ToD_site/settings.py
+++ ToD_site/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crawls.apps.CrawlsConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crawls.apps.CrawlsConfig',
+    'polls.apps.PollsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
